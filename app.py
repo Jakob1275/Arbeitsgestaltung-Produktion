48ca2a12a853af0e156e5984086c62a442fc622b
Jakob1275 <jakob.weber96@web.de>
2025-07-14 11:13:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -479,13 +479,13 @@ def nav_buttons(position):
         if st.session_state.current_tab_index > 0:
             if st.button("← Zurück", key=f"back_{position}_{st.session_state.current_tab_index}"):
                 st.session_state.current_tab_index -= 1
-                st.experimental_set_query_params(tab=st.session_state.current_tab_index)  # optional visuelle Navigation aktualisieren
+                st.query_params(tab=st.session_state.current_tab_index)  # optional visuelle Navigation aktualisieren
                 st.stop()
     with col3:
         if st.session_state.current_tab_index < len(tab_names) - 1:
             if st.button("Weiter →", key=f"next_{position}_{st.session_state.current_tab_index}"):
                 st.session_state.current_tab_index += 1
-                st.experimental_set_query_params(tab=st.session_state.current_tab_index)
+                st.query_params(tab=st.session_state.current_tab_index)
                 st.stop()
 
 # Buttons oben
