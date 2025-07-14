08a7c5a539e89f83fa7af098adf85abb8fe8ef18
Jakob1275 <jakob.weber96@web.de>
2025-07-14 13:21:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -595,12 +595,14 @@ nav_buttons("bottom")
 st.markdown(
     """
     <a href="#top">
-        <button style='position: fixed; bottom: 40px; right: 50%px; transform: translateX(-50%); z-index: 9999;
-                       background-color: #1f77b4; color: white; border: none;
-                       padding: 10px 16px; border-radius: 6px; font-size: 16px;
-                       box-shadow: 0 2px 6px rgba(0,0,0,0.2); cursor: pointer;'>
-            ⬆ Nach oben
-        </button>
+        <div style='position: fixed; bottom: 40px; left: 50%; transform: translateX(-50%);
+                    z-index: 9999;'>
+            <button style='background-color: #1f77b4; color: white; border: none;
+                           padding: 10px 16px; border-radius: 6px; font-size: 16px;
+                           box-shadow: 0 2px 6px rgba(0,0,0,0.2); cursor: pointer;'>
+                ⬆ Nach oben
+            </button>
+        </div>
     </a>
     """,
     unsafe_allow_html=True
