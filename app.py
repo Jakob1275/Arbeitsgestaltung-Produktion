a409db9b044d26e4f2fe1d313cf59470225c3a72
Jakob1275 <jakob.weber96@web.de>
2025-09-24 14:06:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,8 @@ credentials = Credentials.from_service_account_info(service_account_info, scopes
 client = gspread.authorize(credentials)
 
 # Spreadsheet öffnen
-sheet = client.open("Modell_Evaluation").sheet1
+sheet = client.open("Modell_Evaluation").worksheet("Tabellenblatt1")
+
 # Struktur der Anwendung
 st.set_page_config(page_title="Modell zur Systematisierung flexibler Arbeit", layout="wide")
 st.title("Modell zur Systematisierung flexibler Arbeit")
