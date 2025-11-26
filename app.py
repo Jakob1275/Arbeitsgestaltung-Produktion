9ada77dfe0251b9e62cb308b07ab3ff100cc12d2
Jakob1275 <jakob.weber96@web.de>
2025-11-26 08:21:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1608,8 +1608,13 @@ if current_tab == "Evaluation":
     # Einführungstext
     st.markdown("""
         <div style='font-size: 1.1rem; margin-bottom: 1rem;'>
-            Vielen Dank für die Bearbeitung des entwickelten Modells.  
-            Um die Qualität weiter zu verbessern, bitten wir Sie um eine kurze Bewertung.
+            Vielen Dank, dass Sie sich die Zeit genommen haben, das Modell zur Systematisierung flexibler Arbeit in der Zerspanung zu bearbeiten.
+
+            Mit der Bearbeitung des Modells haben Sie mir bereits wertvolle Einblicke in die Flexibilitätsbedarfe Ihres Unternehmens gegeben.
+
+            Nun bitte ich Sie um eine kurze Einschätzung zu Aufbau, Verständlichkeit und Praxisnutzen des Modells selbst. Ihre ehrliche und kritische Rückmeldung ist dabei das wichtigste Ziel: Nur so kann ich das Instrument optimieren und es zu einem wirkungsvollen Werkzeug für die gesamte Branche machen.
+
+            Ihre Antworten bleiben selbstverständlich anonym und werden ausschließlich für die wissenschaftliche Weiterentwicklung verwendet.
         </div>
     """, unsafe_allow_html=True)
 
